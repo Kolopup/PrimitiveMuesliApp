05517edc05ff3ddbd7f2575a69dd9df85cb2a358
Arseniy <senestrov@mail.ru>
2021-10-22 17:08:10 +0300

    Finish

---
+++ ICTSkillsApp/ICTSkillsApp/Partials/Candidates.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ICTSkillsApp.Model
+{
+    partial class Candidates
+    {
+        public int Rank { get; set; } = 0;
+        public int Points { get; set; } = 0;
+    }
+}
---
+++ ICTSkillsApp/ICTSkillsApp/Windows/AddCandidateWindow.xaml.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ICTSkillsApp.Windows
+{
+    /// <summary>
+    /// Interaction logic for AddCandidateWindow.xaml
+    /// </summary>
+    public partial class AddCandidateWindow : Window
+    {
+        Pages.MainMenuPage mainMenuPage;
+        public AddCandidateWindow(Pages.MainMenuPage c)
+        {
+            InitializeComponent();
+            mainMenuPage = c;
+            if (mainMenuPage.DGCompetition.SelectedItem == null)
+            {
+                mainMenuPage.DGCompetition.SelectedIndex = 0;
+            }
+            CBCompetition.ItemsSource = MainWindow.db.Competitions.ToList();
+            CBCompetition.SelectedItem = (Model.Competitions)c.DGCompetition.SelectedItem;
+        }
+        private bool Validate()
+        {
+            string errorResult = "";
+            if (string.IsNullOrWhiteSpace(TBName.Text))
+            {
+                errorResult += "Enter Name\n";
+            }
+            if(string.IsNullOrWhiteSpace(TBCompany.Text))
+            {
+                errorResult += "Enter Company\n";
+            }
+            if(string.IsNullOrWhiteSpace(TBSchool.Text))
+            {
+                errorResult += "Enter School";
+            }
+            if(errorResult != "")
+            {
+                MessageBox.Show(errorResult);
+                return false;
+            }
+            return true;
+        }
+        private void BAddCandidate_Click(object sender, RoutedEventArgs e)
+        {
+            if(Validate())
+            {
+                if(mainMenuPage.DGCompetition.SelectedItem == null)
+                {
+                    mainMenuPage.DGCompetition.SelectedIndex = 0;
+                }
+                Model.Candidates candidates = new Model.Candidates() { Name = TBName.Text, Company = TBCompany.Text, School = TBSchool.Text, CompetitionId = ((Model.Competitions)CBCompetition.SelectedItem).Id};
+                MainWindow.db.Candidates.Add(candidates);
+                MainWindow.db.SaveChanges();
+                mainMenuPage.DGCandidates.ItemsSource = null;
+                mainMenuPage.DGCandidates.ItemsSource = MainWindow.db.Candidates.Where(c=>c.CompetitionId == ((Model.Competitions)mainMenuPage.DGCompetition.SelectedItem).Id).ToList();
+                this.Close();
+            }
+        }
+    }
+}
